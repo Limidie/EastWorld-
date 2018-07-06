fdb8015a41818cd31a6792bc4546782437b32540
李迷迭 <31162908+Limidie@users.noreply.github.com>
2018-07-06 19:28:42 +0800

    Add files via upload

--- pages/select-address/index.js
+++ pages/select-address/index.js
@@ -1,53 +1,65 @@
-var a = getApp();
-
+//index.js
+//获取应用实例
+var app = getApp()
 Page({
-    data: {
-        addressList: []
-    },
-    selectTap: function(t) {
-        var s = t.currentTarget.dataset.id;
-        wx.request({
-            url: "https://api.it120.cc/" + a.globalData.subDomain + "/user/shipping-address/update",
-            data: {
-                token: a.globalData.token,
-                id: s,
-                isDefault: "true"
-            },
-            success: function(a) {
-                wx.navigateBack({});
-            }
-        });
-    },
-    addAddess: function() {
-        wx.navigateTo({
-            url: "/pages/address-add/index"
-        });
-    },
-    editAddess: function(a) {
-        wx.navigateTo({
-            url: "/pages/address-add/index?id=" + a.currentTarget.dataset.id
-        });
-    },
-    onLoad: function() {
-        console.log("onLoad");
-    },
-    onShow: function() {
-        this.initShippingAddress();
-    },
-    initShippingAddress: function() {
-        var t = this;
-        wx.request({
-            url: "https://api.it120.cc/" + a.globalData.subDomain + "/user/shipping-address/list",
-            data: {
-                token: a.globalData.token
-            },
-            success: function(a) {
-                0 == a.data.code ? t.setData({
-                    addressList: a.data.data
-                }) : 700 == a.data.code && t.setData({
-                    addressList: null
-                });
-            }
-        });
-    }
-});
+  data: {
+    addressList: []
+  },
+
+  selectTap: function (e) {
+    var id = e.currentTarget.dataset.id;
+    wx.request({
+      url: 'https://api.it120.cc/' + app.globalData.subDomain + '/user/shipping-address/update',
+      data: {
+        token: wx.getStorageSync('token'),
+        id: id,
+        isDefault: 'true'
+      },
+      success: (res) => {
+        wx.navigateBack({})
+      }
+    })
+  },
+
+  addAddess: function () {
+    wx.navigateTo({
+      url: "/pages/address-add/index"
+    })
+  },
+
+  editAddess: function (e) {
+    wx.navigateTo({
+      url: "/pages/address-add/index?id=" + e.currentTarget.dataset.id
+    })
+  },
+
+  onLoad: function () {
+    console.log('onLoad')
+
+
+  },
+  onShow: function () {
+    this.initShippingAddress();
+  },
+  initShippingAddress: function () {
+    var that = this;
+    wx.request({
+      url: 'https://api.it120.cc/' + app.globalData.subDomain + '/user/shipping-address/list',
+      data: {
+        token: wx.getStorageSync('token')
+      },
+      success: (res) => {
+        if (res.data.code == 0) {
+          that.setData({
+            addressList: res.data.data
+          });
+        } else if (res.data.code == 700) {
+          that.setData({
+            addressList: null
+          });
+        }
+      }
+    })
+  }
+
+})
